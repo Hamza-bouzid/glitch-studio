7e251c841474e77d3f1ba44206b2ad6597a15792
Hamza-bouzid <hamzadeveloper1@gmail.com>
2021-02-06 18:11:13 +0100

    completato live section

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 // NAVBAR
-const selectElement = (element) => document.querySelector(element);
-
+const selectElement = element => document.querySelector(element);
 selectElement('.mobile-menu').addEventListener('click', () => {
   selectElement('.nav-header').classList.toggle('active');
+});
+// SMOOTH SCROOL
+const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 800,
 });
