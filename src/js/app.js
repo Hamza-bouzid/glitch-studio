bc2d09cc3da69b3df46f0efb1f83c008975773d2
Hamza-bouzid <hamzadeveloper1@gmail.com>
2021-02-14 13:25:51 +0100

    footer completo v1

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,34 @@
 // NAVBAR
 const selectElement = element => document.querySelector(element);
+const closeNavbar = document.querySelector('.close-navbar');
+
 selectElement('.mobile-menu').addEventListener('click', () => {
   selectElement('.nav-header').classList.toggle('active');
 });
+selectElement('#logo-nav').addEventListener('click', () => {
+  selectElement('.nav-header').classList.remove('active');
+});
+selectElement('#link1-nav').addEventListener('click', () => {
+  selectElement('.nav-header').classList.toggle('active');
+});
+selectElement('#link2-nav').addEventListener('click', () => {
+  selectElement('.nav-header').classList.toggle('active');
+});
+selectElement('#link3-nav').addEventListener('click', () => {
+  selectElement('.nav-header').classList.toggle('active');
+});
+selectElement('#link4-nav').addEventListener('click', () => {
+  selectElement('.nav-header').classList.toggle('active');
+});
+selectElement('#link5-nav').addEventListener('click', () => {
+  selectElement('.nav-header').classList.toggle('active');
+});
+selectElement('#link6-nav').addEventListener('click', () => {
+  selectElement('.nav-header').classList.toggle('active');
+});
+
+
 // SMOOTH SCROOL
 const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 800,
+  speed: 900,
 });
